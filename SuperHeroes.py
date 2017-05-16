4a2b0114a3cd37b2fde39de23cec1ffe58d4a26a
Jari Voutilainen <jari.voutilainen@iki.fi>
2017-05-16 15:43:10 +0300

    update the code

--- SuperHeroes.py
+++ SuperHeroes.py
@@ -15,33 +15,23 @@ spark = SparkSession  \
 sc = spark.sparkContext
 data = sc.textFile("hero-comic-network.csv")
 
-#data = ['"FROST, CARMILLA","AA2 35"', '"KILLRAVEN/JONATHAN R","AA2 35"']
-
-#heroes = sc.parallelize(data)
 
 edge_data = data.map(lambda row: next(csv.reader(row.splitlines(), skipinitialspace=True)))\
     .map(lambda x: (x[1], x[0])).distinct()
 
-#edge_data.saveAsTextFile("flipped")
 
-combined = edge_data.groupByKey()\
+listOfConnections = edge_data.groupByKey()\
     .mapValues(list)\
     .map(lambda x: x[1])
 
-sorted_list = combined.map(lambda x: sorted(x))
-
-#combined.saveAsTextFile("combined")
-
-pairs = sorted_list.flatMap(lambda x: list(combinations(x, 2)))
-
-edge_count = pairs.map(lambda x: (x, 1)).reduceByKey(lambda a,b: a+b).map(lambda x: (x[0][0], x[0][1], x[1]))
-#pairs.saveAsTextFile("cartesian")
+sortedListOfConnections = listOfConnections.map(lambda x: sorted(x))
 
 
+connectionPairs = sortedListOfConnections.flatMap(lambda x: list(combinations(x, 2)))
+
+connectionPairCount = connectionPairs.map(lambda x: (x, 1)).reduceByKey(lambda a,b: a+b).map(lambda x: (x[0][0], x[0][1], x[1]))
 
 heroes = edge_data.map(lambda x: (x[1],)).distinct()
-#comics = edge_data.map(lambda x: (x[1], "comic"))
-#formatted_data = heroes.union(comics)
 
 
 
@@ -52,19 +42,13 @@ edgeSchema = StructType([StructField("src", StringType(), False),
                          StructField("dst", StringType(), False),
                          StructField("connections", IntegerType(), False)])
 
-edges = spark.createDataFrame(edge_count, edgeSchema)
+edges = spark.createDataFrame(connectionPairCount, edgeSchema)
 
-graph_with_connections = GraphFrame(vertices, edges)
+graph = GraphFrame(vertices, edges)
 
+graph.degrees.sort("degree", ascending=False).show()
+graph.degrees.filter("id == 'CAPTAIN AMERICA'").show()
 
-edge_without_connectionsSchema = StructType([StructField("src", StringType(), False),
-                         StructField("dst", StringType(), False)])
-edges_without_connections = spark.createDataFrame(pairs, edge_without_connectionsSchema)
-
-graph_without_connections = GraphFrame(vertices, edges_without_connections)
-
-pagerank_with_connections = graph_with_connections.pageRank(resetProbability=0.15, tol=0.01)
-pagerank_with_connections.vertices.sort(desc("pagerank")).limit(100).write.save("pagerank_with_connections", format="csv")
-
-pagerank_without_connections = graph_without_connections.pageRank(resetProbability=0.15, tol=0.01)
-pagerank_without_connections.vertices.sort(desc("pagerank")).limit(100).write.save("pagerank_without_connections", format="csv")
+sc.setCheckpointDir(".")
+results = graph.connectedComponents()
+results.filter("component != 0").write.save("disconnected", format="csv")
